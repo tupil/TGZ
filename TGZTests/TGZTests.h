e7e3dab8e199b5db8f48375fb5632934ee176709
Ben Lenarts <ben@lenarts.nl>
2011-12-06 17:18:29 +0100

    Initial Commit

---
+++ TGZTests/TGZTests.h
@@ -0,0 +1,13 @@
+//
+//  TGZTests.h
+//  TGZTests
+//
+//  Created by Ben Lenarts on 06-12-11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TGZTests : SenTestCase
+
+@end
---
+++ TGZTests/TGZTests.m
@@ -0,0 +1,32 @@
+//
+//  TGZTests.m
+//  TGZTests
+//
+//  Created by Ben Lenarts on 06-12-11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TGZTests.h"
+
+@implementation TGZTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TGZTests");
+}
+
+@end
